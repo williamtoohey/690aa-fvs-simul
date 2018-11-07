c9de531091710c58446b673083679550505e287c
William Toohey <williamtoohey7@gmail.com>
2018-11-07 10:26:04 -0500

    added brute-force method to get optimal fvs for graph

--- fvs.py
+++ fvs.py
@@ -1,5 +1,9 @@
 import networkx as nx
-import sys
+import itertools as it
+import random as ran
+import time
+
+import math
 
 # gets all cycles of size >= 3 in graph - converting from undirected to directed makes this easier with using networkx
 def get_cycles(graph):
@@ -34,10 +38,32 @@ def verify_fvs(graph, fvs):
 			return False
 	return True
 
+# returns the optimal fvs for graph - checks all possible selections of vertices and keeps track of the best fvs seen so far
+def optimal_fvs(graph):
+	cycles = get_cycles(graph)
+	optSol = []
+	optWeight = math.inf
+	size = len(graph.nodes())
+	vertices = set().union(*cycles)
+	for x in range(1, size - 1):
+		combo = it.combinations(vertices, x)
+		for y in combo:
+			if verify_fvs(graph, y):
+				weight = 0
+				for z in y:
+					weight += weights[z]
+				if weight < optWeight:
+					optWeight = weight
+					optSol = y
+	return optSol, optWeight
+
+
 # first argument is |V| in g, second argument is probability of any edge being in g
-g = nx.erdos_renyi_graph(4, 0.5)
-print(g.edges())
-cycles = get_cycles(g)
-for c in cycles:
-	print(c)
-	print(is_semidisjoint(g, c))
+g = nx.erdos_renyi_graph(7, 1)
+size = len(g.nodes())
+weights = [ran.randint(1, size) for _ in range(size)]
+t1 = time.time()
+sol, weight = optimal_fvs(g)
+t2 = time.time()
+elapsed = t2 - t1
+print(elapsed)
